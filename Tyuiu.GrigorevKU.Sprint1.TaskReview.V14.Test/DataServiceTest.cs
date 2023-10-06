e19f07612e2a604fca93ac1f19d73ffc765fafa9
Tyuiu.GrigorevKU@outlook.com <uhbujhmtdrjcnz@gmail.com>
2023-10-06 12:21:21 +0500

    Add project files.

---
+++ Tyuiu.GrigorevKU.Sprint1.TaskReview.V14.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.GrigorevKU.Sprint1.TaskReview.V14.Lib;
+namespace Tyuiu.GrigorevKU.Sprint1.TaskReview.V14.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double wait = 2.536;
+            double res = ds.Calculate(1, 1);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.GrigorevKU.Sprint1.TaskReview.V14/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.GrigorevKU.Sprint1.TaskReview.V14.Lib;
+namespace Tyuiu.GrigorevKU.Sprint1.TaskReview.V14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1| Выполнил: Григорьев К. Ю. | ИИПб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Создание итогового решения по спринту                             *");
+            Console.WriteLine("* Задание Спринт1Ревью                                                    *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнил Григорьев К. Ю. | ИИПб-23-2                                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать консольную программу на С#, которая вычисляет математическое   *");
+            Console.WriteLine("* выражение по исходным значениям данных, вводимых пользователем.         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+            double x;
+            double y;
+            Console.WriteLine("Введите значение x:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение y:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("2^(-x) + (5x^2/3x^3) - cosx^2 + sin(2xy) = " + ds.Calculate(x, y), 3);
+            Console.ReadKey();
+        }
+    }
+}
